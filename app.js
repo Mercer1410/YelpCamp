304efdc3e91dba4fe80cdeaadfa9b75206bcf45b
Mercer1410 <hs7582@srmist.edu.in>
2020-05-05 17:20:34 +0530

    add dburl

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ var commentRoutes 		= require("./routes/comments"),
 	campgroundRoutes 	= require("./routes/campgrounds"),
 	indexRoutes 			= require("./routes/index")
 
-mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.createConnection("mongodb://localhost/yelp_camp_v4", { useNewUrlParser: true, useUnifiedTopology: true });
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp";
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
