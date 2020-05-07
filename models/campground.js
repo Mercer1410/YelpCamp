08240d9854fcdc0521470ec86008e5f1ba5e0645
Mercer1410 <hs7582@srmist.edu.in>
2020-05-07 21:29:52 +0530

    Time Stamp Added

--- models/campground.js
+++ models/campground.js
@@ -5,6 +5,7 @@ var campgroundSchema = new mongoose.Schema({
 	cost: Number,
 	image: String,
 	description: String,
+	createdAt: {type: Date, default: Date.now},
 	author: {
 		id:{
 			type: mongoose.Schema.Types.ObjectId,
