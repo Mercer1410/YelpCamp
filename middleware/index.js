3a98fc884afe6dcf9b9a48440782ac7fd6a2a40c
Mercer1410 <hs7582@srmist.edu.in>
2020-05-07 22:49:48 +0530

    Admin roles Added

--- middleware/index.js
+++ middleware/index.js
@@ -12,7 +12,7 @@ middlewareObj.checkOwnership = function(req,res,next){
 				res.redirect("back");
 			}else{
 				//does the user own the campground
-				if(foundCampground.author.id.equals(req.user._id)){
+				if(foundCampground.author.id.equals(req.user._id) || req.user.isAdmin){
 					next();
 				}else{
                     req.flash("error", "You don't Own this Campground.");
@@ -33,7 +33,7 @@ middlewareObj.checkCommentOwnership = function(req,res,next){
 				res.redirect("back");
 			}else{
 				//does the user own the comment
-				if(foundComment.author.id.equals(req.user._id)){
+				if(foundComment.author.id.equals(req.user._id) || req.user.isAdmin){
 					next();
 				}else{
                     req.flash("error", "You don't own this comment.");
